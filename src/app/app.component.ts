35e620d36e01dbda174b30563b388413638430c6
gwinbleid <corsar909@gmail.com>
2019-01-17 16:24:50 +0300

    local commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem  } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dragAndDrop';
+
+  todo = [
+    "Max",
+    "Nicole",
+    'Aysek'
+  ];
+
+  done = [
+    "Gregory",
+    'Sandor',
+    'James'
+  ];
+
+  thirdArray = [
+    "Sercei",
+    "Eddard",
+    "Robert"
+  ];
+
+  containerArray = [
+    [ "Sercei", "Eddard", "Robert" ],
+    [ "Adam", "Gerhard", "Scmidth" ],
+    [ "Neo", "Lans", "Herbert" ]
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+  getLinkOfConnectedArrays() {
+
+  }
+
+  createDynamicConnectedList(i) {
+    const arr = this.containerArray
+            .map((el, index) => {
+              if (typeof(el) === 'object') {
+                return {name: 'item-', id: index};
+              }
+            })
+            .filter(el => (typeof(el) === 'object' && el.id !== i) ? true : false )
+            .map(item => item.name + item.id);
+
+    console.log(arr);
+    return [...arr, 'main'];
+  }
+
+  addArray() {
+    this.containerArray = [...this.containerArray, []];
+  }
+
+  createListForMain() {
+    return this.containerArray
+      .map((el, index) => {
+        if (typeof(el) === 'object') {
+          return {name: 'item-', id: index};
+        }
+      })
+      .filter(el => typeof(el) === 'object')
+      .map(item => item.name + item.id);
+  }
+
+  checkItemIsObject(item) {
+    return typeof(item) === 'object';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
